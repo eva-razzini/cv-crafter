773615ea183d2e5aa4b26977d9dd7b8eeba2b12a
Serge Razzini <eva.razzini@laplateforme.io>
2023-09-11 10:18:28 +0200

    page connexion, index, inscription, profil

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Pixel</title>
+  <link id="style" rel="stylesheet" type="text/css" href="style6.css">
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&display=swap');
+  </style>
+</head>
+
+<body>
+    <h1>Bienvenue</h1>
+    <button><a href="connexion.php">Connexion</button>
+    <button><a href="inscription.php">Inscription</button>
+</body>
+
+</html>
