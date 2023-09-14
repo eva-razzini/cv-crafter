082ddf065c5005d788db9df28c3e9ab0dbac9db0
Serge Razzini <eva.razzini@laplateforme.io>
2023-09-14 13:40:15 +0200

    débug importation photo

--- profil.php
+++ profil.php
@@ -13,10 +13,11 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Récupérer les informations de l'utilisateur connecté
-    $query = "SELECT login, prenom, nom, phone, postal, ville, photo FROM utilisateurs WHERE login = ?";
+    $query = "SELECT login, prenom, nom, password, phone, postal, ville, photo FROM utilisateurs WHERE login = ?";
     $stmt = $conn->prepare($query);
     $stmt->execute([$_SESSION["login"]]);
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
 
     // Vérifier si le formulaire de mise à jour a été soumis
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -28,15 +29,16 @@ try {
         $newphone = $_POST["phone"];
         $newpostal = $_POST["postal"];
         $newville = $_POST["ville"];
-        $newphoto = $_POST["photo"];
+        $newphoto = $_FILES["photo"];
     
 
            // Vérifie si le fichier a été uploadé sans erreur.
-        if(isset( $newphoto) &&  $newphoto["error"] == 0){
             $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
-            $filename = $newphoto["name"];
-            $filetype = $newphoto["type"];
-            $filesize = $newphoto["size"];
+            $filename = $_FILES["photo"]["name"];
+            $filetype = $_FILES["photo"]["type"];
+            $filesize = $_FILES["photo"]["size"];
+
+            var_dump("holaaa",$_FILES["photo"]["name"]);
 
             // Vérifie l'extension du fichier
             $ext = pathinfo($filename, PATHINFO_EXTENSION);
@@ -46,33 +48,23 @@ try {
             $maxsize = 5 * 1024 * 1024;
             if($filesize > $maxsize) die("Error: La taille du fichier est supérieure à la limite autorisée.");
 
-            // Vérifie le type MIME du fichier
-            if(in_array($filetype, $allowed)){
-                // Vérifie si le fichier existe avant de le télécharger.
-                if(file_exists("upload/" . $newphoto["name"])){
-                    echo     $newphoto["name"] ." existe déjà.";
-                } else{
                     // Mettre à jour les informations de l'utilisateur dans la base de données
             $updateQuery = "UPDATE utilisateurs SET login = ?, prenom = ?, nom = ?, password = ?, phone = ?, postal = ?, ville = ?, photo = ?  WHERE login = ?";
             $updateStmt = $conn->prepare($updateQuery);
-            $updateStmt->execute([$newLogin, $newPrenom, $newNom, $newPassword, $newphone, $newpostal, $newville, $newphoto, $_SESSION["login"]]);
+            $updateStmt->execute([$newLogin, $newPrenom, $newNom, $newPassword, $newphone, $newpostal, $newville, $_FILES["photo"]["name"], $_SESSION["login"]]);
 
             // Mettre à jour le login de l'utilisateur dans la variable de session
             $_SESSION["login"] = $newLogin;
 
         
-                    move_uploaded_file($newphoto["tmp_name"], "../upload" . $newphoto["name"]);
+                    move_uploaded_file($_FILES["photo"]["tmp_name"], "upload/" . $_FILES["photo"]["name"]);
                     echo "Votre fichier a été téléchargé avec succès.";
                     // Redirection vers la page de profil mise à jour
             header("Location: profil.php");
+            echo "<img src='upload/".$row['photo']."'>";
+
             exit;
-                } 
-            } else{
-                echo "Error: Il y a eu un problème de téléchargement de votre fichier. Veuillez réessayer."; 
-            }
-        } else{
-            echo "Error: " . $newphoto["error"];
-        }
+
 
     }
 } catch (PDOException $e) {
@@ -103,8 +95,9 @@ if (isset($_GET["logout"])) {
   </style>
 </head>
 <body class="bodyprof">
-    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>" enctype="multipart/form-data">
         <h1>Profil</h1>
+        <!-- Display the user's current login and other fields here -->
         <label for="login" style="font-size: 0.5em">Login:</label>
         <input type="text" id="login" name="login" value="<?php echo $row["login"]; ?>" required><br>
 
@@ -123,11 +116,25 @@ if (isset($_GET["logout"])) {
         <label for="ville" style="font-size: 0.5em">Ville:</label>
         <input type="text" id="ville" name="ville" value="<?php echo $row["ville"]; ?>" required><br>
 
+        <!-- Display the default profile photo -->
+        <?php //if (empty($row["photo"])) : ?>
+            <!-- <img src="upload/profil_default.jpg" alt="Default Profile Photo" width="150"> -->
+        <?php //else : ?>
+            <?php //echo '<img src="upload/'.$row["photo"].'" width="128" height="117"> </img>' ?>
+            <?php
+
+?>
+
+    <img src=upload/<?=$row['photo']?> height="430">
+
+              <?php //endif; ?>
+
+        <!-- Allow the user to upload a new photo -->
         <label for="fileUpload" style="font-size: 0.5em">Photo de profil:</label>
-        <input type="file" id="fileUpload" name="photo" value="<?php echo $row["photo"]; ?>" required><br>
+        <input type="file" id="fileUpload" name="photo" required><br>
 
         <label for="password" style="font-size: 0.5em">Nouveau mot de passe:</label>
-        <input type="password" id="password" name="password" required><br>
+        <input type="text" id="password" name="password"><br>
 
         <input type="submit" value="Enregistrer les modifications">
     </form>
